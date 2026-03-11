a0b4eb71b5391878d821e7b68ba3e7be366df593
Apolo Dev <266833479+apolodevfivem@users.noreply.github.com>
2026-03-11 10:55:29 -0300

    Alterando o fx manifest porque pensei numa ideia nova de fazer a compatibilidade com todas as bases, por config mesmo

--- client.lua
+++ client.lua
@@ -1,10 +1,16 @@
 -- Apolo Dev --
 
+local WallAtivado = false
+
 Citizen.CreateThread(function()
     while true do
         local ApoloDev = 1000
 
-        
+        if WallAtivado then
+            ApoloDev = 5
+
+            
+        end
 
         Wait(ApoloDev)
     end
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,13 +4,11 @@ game "gta5"
 author "ApoloDev"
 
 client_scripts {
-    "@vrp/lib/Utils.lua",
     "config.lua",
     "client.lua"
 }
 
 server_scripts {
-    "@vrp/lib/Utils.lua",
     "config.lua",
     "server.lua"
 }
