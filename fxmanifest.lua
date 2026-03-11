b157c6fab2ef27c8d5e655c3b0023e2259ea5926
Apolo Dev <266833479+apolodevfivem@users.noreply.github.com>
2026-03-11 10:45:22 -0300

    Commit inicial do wall

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "bodacious"
+game "gta5"
+
+author "ApoloDev"
+
+client_scripts {
+    "@vrp/lib/Utils.lua",
+    "config.lua",
+    "client.lua"
+}
+
+server_scripts {
+    "@vrp/lib/Utils.lua",
+    "config.lua",
+    "server.lua"
+}
