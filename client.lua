9c33856755bbf71e2cddd1dd1fd9907ce2d67371
Apolo Dev <266833479+apolodevfivem@users.noreply.github.com>
2026-03-11 10:59:23 -0300

    Finalizado a ideia que tive sobre a forma de pegar as informações do player tipo emprego ou id e etc

--- client.lua
+++ client.lua
@@ -1,6 +1,7 @@
 -- Apolo Dev --
 
 local WallAtivado = false
+local ServerInfos = {} -- Adicionado para quando quiser trazer informações sobre qualquer coisa sobre qualquer entidade é só solicitar de tempos em tempos
 
 Citizen.CreateThread(function()
     while true do
@@ -9,9 +10,14 @@ Citizen.CreateThread(function()
         if WallAtivado then
             ApoloDev = 5
 
-            
+
         end
 
         Wait(ApoloDev)
     end
+end)
+
+RegisterNetEvent("Staff:ReceberInfos")
+AddEventHandler("Staff:ReceberInfos", function(Data)
+    ServerInfos = Data
 end)
